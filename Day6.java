b81bea97777e1f11c6ffe9c772fa25ae1e3b15e4
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-03-30 00:00:30 +0530

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class Day6{
+    public Static void main(String[] args)
+
+    {
+        String Customer= "Joyous";
+        int units = 26;
+        double costperunits = 5.6;
+        System.out.println("Customer:" + Customer);
+        System.out.println("units:" + units);
+        System.out.println("amount" + costperunits );
+
+    }
+}
