8ffcef2a4f5697b16935a224e0d1b2d47320f342
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-03-30 01:27:29 +0530

    Add files via upload

---
+++ trial.java
@@ -0,0 +1,10 @@
+public class trial{
+    public static void main(String[] args)
+    {
+        int Score = 32;
+        int Total = 50;
+
+        double Totalscore = (double) Score / Total * 100.0d;
+        System.out.println("ur scoree is : " + Totalscore);
+    }
+}
