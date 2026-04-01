e8d6b145d59fbc86753e3b7001c892a1d6a8353b
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-04-01 23:51:43 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,19 @@
+public class Marks{
+    public static void main(String[] args){
+        int mark = 48;
+        if(mark >= 50){
+            if(mark >=90){
+                System.out.println("Grade O");
+            }
+            else if(mark >= 80){
+                System.out.println("Grade A");
+            }
+            else{
+                System.out.println("Grade B");
+            }
+        }
+        else{
+            System.out.println("Sorry Youre Fail my Bruh");
+        }
+    }
+}
