261556457e13bcac2f5a6de796ba80125e9b9f69
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-04-01 23:59:39 +0530

    Add files via upload

---
+++ Marks1.java
@@ -0,0 +1,9 @@
+public class Marks1{
+    public static void main(String[] args){
+        int p = 1000;
+        int r = 20;
+        int time = 3;
+        double simplInterest = (p*r*time ) /100.0 ;
+        System.out.println("Simpl Interest: " + simplInterest);
+    }
+}
