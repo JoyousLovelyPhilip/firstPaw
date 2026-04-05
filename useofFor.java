738ebc1eaf783387cd41dcdfaab61e080c775743
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-04-05 23:55:39 +0530

    Add files via upload

---
+++ useofFor.java
@@ -0,0 +1,14 @@
+public class useofFor {
+    public static void main(String[] args)
+    {
+        for (int i =0; i <= 10; i++){
+            if(i%2 ==0){
+                System.out.println("Number is even");
+            }
+            else {
+                System.out.println("Number is odd");
+            }
+        }
+
+    }
+}
