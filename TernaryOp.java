9f540e5b976b2238157c8c4364d28d37a2e32ccb
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-04-10 22:22:47 +0530

    Add files via upload

---
+++ TernaryOp.java
@@ -0,0 +1,9 @@
+public class TernaryOp{
+    public static void main(String[] args){
+        int a = 34;
+        int b = 26;
+
+        int max = (a  > b)?a:b;
+        System.out.println("The max number is: " + max);
+    }
+}
