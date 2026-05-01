eac1ddca325331460049c77f81f1c438578aefc3
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-01 22:27:04 +0530

    Add files via upload

---
+++ RainTrap.java
@@ -0,0 +1,12 @@
+public class RainTrap{
+    public static void main(String[] args){
+        int[] height = {0,3,1,6,0,3};
+        int left =0, right =height.length -1;
+        int leftMax = 0; rightMax = 0 ;
+        int water = 0;
+
+        while(left <= right){
+            
+        }
+    }
+}
