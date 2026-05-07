d2fec43cd9b21e457d520b5177acea7e0beb1e6f
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-07 21:49:11 +0530

    Add files via upload

---
+++ CheckMap.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+public class CheckMap{
+    public static void main(String[] args){
+        HashMap<String, String> JoyTry = new HashMap<String, String>();
+
+        JoyTry.put("Joyous","Happiness");
+        JoyTry.put("Annet","The First Fruit");
+        JoyTry.put("lovely","Loveliest to Soul");
+        JoyTry.put("Philip V Pandian", "oopsie! I dono the meaning, but Absolutely the Ruler of Our Family nxt to God");
+
+        System.out.println("THE HASHMAP SAMPLE:" + JoyTry);
+    }
+}
