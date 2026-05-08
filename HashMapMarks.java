5e05c6537b0aa1e46354a8c317e22ab1838362f4
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-08 23:44:34 +0530

    Add files via upload

---
+++ HashMapMarks.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+public class HashMapMarks{
+    public static void main(String[] args){
+        HashMap<String, Integer> marks = new HashMap<String, Integer>();
+
+        marks.put("janu",210);
+        marks.put("malllu", 350);
+        marks.put("giga",370);
+        marks.put("dhanseh",80);
+        marks.put("abi", 100);
+        marks.put("allwi", 340);
+        marks.put("puthu", 450);
+
+        System.out.println("The Marks are: " +marks);
+        System.out.println("Mark of Dhanush : " + marks.get("dhanseh"));
+    }
+}
