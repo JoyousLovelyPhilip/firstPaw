03ec709b8c572489d5961c4e6c0db7c3d1bb1681
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-10 23:28:38 +0530

    Fix congratulatory message formatting in accountstatus

--- DAY3.java
+++ DAY3.java
@@ -5,7 +5,7 @@ class Bank{
 }
     void accountstatus()
 {
-    System.out.println("Congratulations aadhaar and kRC attached");
+    System.out.println("Congratulations! aadhaar and kRC attached");
 } 
 }
 class joooi extends Bank{
