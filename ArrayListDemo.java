df937cff2b35aba10a24d9b9a8983f023326d376
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-13 22:43:34 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class ArrayListDemo{
+    public static void main(String[] args){
+        ArrayList<String> vandis = new ArrayList<String>();
+        
+        vandis.add("Volvo");
+        vandis.add("Audi");
+        vandis.add("BMW");
+        vandis.add("Rolls Roys");
+
+        System.out.println(vandis);
+    }
+}
