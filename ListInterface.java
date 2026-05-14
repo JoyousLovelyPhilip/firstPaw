8a0fa3ae4bb7053bd20d1cb0ed8bd4390b778a3f
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-14 16:03:23 +0530

    Add files via upload

---
+++ ListInterface.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+public class ListInterface{
+public static void main(String[] args){
+List<String> vandis = new ArrayList<>();
+        vandis.add("Volvo");
+        vandis.add("Audi");
+        vandis.add("BMW");
+        vandis.add("Rolls Roys");
+
+        System.out.println(vandis);
+}
+}
