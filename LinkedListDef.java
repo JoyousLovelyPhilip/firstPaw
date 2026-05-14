52a88714a51b5204024b59eb6dafd3f674e8c876
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-14 23:57:56 +0530

    Add files via upload

---
+++ LinkedListDef.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class LinkedListDef{
+    public static void main(String[] args){
+
+        LinkedList<String> joy = new LinkedList<String>();
+        joy.add("Volvo");
+        joy.add("Audi");
+        joy.add("BMW");
+        joy.add("Rolls Roys");
+
+        System.out.println(joy);
+    
+    }
+}
