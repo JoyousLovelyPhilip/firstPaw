d8c4d69da4450d70a478463681a7d82013da662e
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-14 15:54:22 +0530

    Add files via upload

---
+++ ArrayListSort.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class ArrayListInt{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> StudentNos = new ArrayList <Integer>();
+
+        System.out.println("Enter the Number of Student with Roll Numbers: ");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter the Roll NUmber of the Students:");
+        for(int i=0; i< num ;i++){
+            int rolls = sc.nextInt();
+            StudentNos.add(rolls);
+        }
+        System.out.println("\nThe List Of Sorted Students are:");
+        Collections.sort(StudentNos);
+        for(Integer display : StudentNos){
+            System.out.println(display);
+        }
+    }
+}
