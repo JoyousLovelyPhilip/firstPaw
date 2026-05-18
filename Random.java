e1fa60d5788c53345e66bfbddd42c5c17a333490
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-19 00:02:36 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,8 @@
+public class MathOps{
+    public static void main(String[]args){
+        System.out.println("The max is :" + Math.random());
+        System.out.println("The max is :" + Math.random() * 189);
+        //The max is :0.5736888399513476, returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+//The max is :97.02011767182886 -To get more control over the random number, for example, if you only want a random number between 0 and 100, you can use the following formula:
+    }
+}
