6f97578f241bee1d200a3ae3618cbf3a172744f6
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-18 23:57:29 +0530

    Add files via upload

---
+++ MathRounds.java
@@ -0,0 +1,11 @@
+public class MathRound
+{
+    public static void main(String[]args){
+        System.out.println("The Round off :" + Math.round(3.6));
+        System.out.println("The ceil off :" + Math.ceil(3.4));
+        System.out.println("The floor off :" + Math.floor(3.4));
+        //The Round off :4
+//The ceil off :4.0
+//The floor off :3.0
+    }
+}
