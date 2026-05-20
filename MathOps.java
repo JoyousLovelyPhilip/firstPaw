52364b042e73c9e9f5e8fbf685489f7d256cec45
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-20 21:04:01 +0530

    Update MathOps.java

--- MathOps.java
+++ MathOps.java
@@ -2,7 +2,7 @@ public class MathOps{
     public static void main(String[]args){
         System.out.println("The max is :" + Math.max(3,4));
         System.out.println("The min is :" + Math.min(3,4));
-        System.out.println("The power is :" + Math.pow(3,4));
+        System.out.println("The power is :" + Math.pow(3,4)); 
         System.out.println("The absolute value is :" + Math.abs(- 3.4));
         System.out.println("The square root  is :" + Math.sqrt(39));
     }
