20116acf6d6fa71862b8063fbe47761549aca7c2
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 15:55:54 +0530

    Add files via upload

---
+++ BoolStoreResult.java
@@ -0,0 +1,10 @@
+public class boolCompare{
+    public static void main(String[] args){
+        int x = 23;
+        int o = 45;
+        boolean isGreater = x > o;
+        System.out.println(isGreater);// false
+        
+        
+    }
+}
