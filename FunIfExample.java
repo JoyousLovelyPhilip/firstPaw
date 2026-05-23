e94edfccc8ddb05eae404e46c0c4ced889e8c637
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 16:53:57 +0530

    Add files via upload

---
+++ FunIfExample.java
@@ -0,0 +1,12 @@
+public class joy {
+    public static void main(String[] args){
+        
+        int doorCode = 1337;
+
+        if (doorCode == 1328) {
+        System.out.println("Correct door code. The door is now open.");
+        } else {
+        System.out.println("Sorry, Wrong code. The door remains closed.");
+        }
+    }
+}//Sorry, Wrong code. The door remains closed.
