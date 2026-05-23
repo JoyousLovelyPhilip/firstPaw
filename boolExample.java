73a24ca99658a83d4125ffeeaaa67c8ec7de77d4
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 15:44:08 +0530

    Add files via upload

---
+++ boolExample.java
@@ -0,0 +1,9 @@
+public class boolExample{
+    public static void main(String[] args){
+        boolean isJoyGoodGirl = true;
+        boolean isJoyLazyGal= false;
+
+        System.out.println("Is Joy a Good Girl: " +isJoyGoodGirl);
+        System.out.println("IS Joy A Lazy brat :" +isJoyLazyGal);
+    }
+}
