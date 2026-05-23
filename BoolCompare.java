dacdb4a13a4dc669de6c7d71e9a879362fd04d9b
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 15:51:26 +0530

    Add files via upload

---
+++ BoolCompare.java
@@ -0,0 +1,11 @@
+public class boolCompare{
+    public static void main(String[] args){
+        int x = 23;
+        int o = 45;
+
+        System.out.println(x < o);//true
+        System.out.println(x== 21);//false
+        
+        
+    }
+}
