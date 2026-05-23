4ff36ad0b167d77d1e55c105cc291e7dce80bcc9
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 16:31:02 +0530

    Add files via upload

---
+++ TernaryIfElse.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[] args){
+        int Age = 34;
+
+        String result= (Age > 18)? "Can Vote" : "Cant Vote";
+        System.out.println(result);//Can Vote
+    }
+}
