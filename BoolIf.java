fab610b916eaff0d223f53e0afebba8bc4112980
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-23 16:24:30 +0530

    Add files via upload

---
+++ BoolIf.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String[] args){
+        boolean LightsOn = true;
+        boolean LightsOff = false;
+        if(LightsOn){
+        System.out.println("The Light is ON");
+        }
+        
+    }
+}
