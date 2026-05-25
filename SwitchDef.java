d4f351f2f81f85201189aae1e0afdc33a33bf8b6
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-25 23:52:55 +0530

    Add files via upload

---
+++ SwitchDef.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class SwitchEg{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Day: ");
+        int day = sc.nextInt();
+
+        switch(day)//The switch should in small ...capital S is error
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2: 
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5: 
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;// output:Enter the Day: 6, Saturday
+            default :
+                System.out.println("Please Enter the Valid NUmber! ");
+        }
+
+    }
+} 
