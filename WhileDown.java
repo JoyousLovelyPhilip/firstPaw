1cbcc43bbe68e03bbc366513a902e5ffc442d8fe
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-27 21:57:00 +0530

    Add files via upload

---
+++ WhileDown.java
@@ -0,0 +1,10 @@
+public class WhileDown{
+    public static void main(String[] args){
+        int countdown = 10;
+        while(countdown > 0){
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy Anniversary My Love!!");// 10,9,8,7,6,5,4,3,2,1 Happy Anniversary My Love
+    }
+}
