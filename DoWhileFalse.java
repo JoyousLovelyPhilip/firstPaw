25e218060c1731c99ced53e041a942991145564a
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-27 23:09:52 +0530

    Add files via upload

---
+++ DoWhileFalse.java
@@ -0,0 +1,11 @@
+public class dowhileDef{
+    public static void main(String[] args){
+        int i = 10;
+        do{
+            System.out.println("The value is: " + i);
+            i++;
+        }
+        while(i < 5);
+    }
+
+}
