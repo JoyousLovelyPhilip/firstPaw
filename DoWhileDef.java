d1f733052052b0067e806028e7ab385a87bc6374
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-27 22:39:29 +0530

    Add files via upload

---
+++ DoWhileDef.java
@@ -0,0 +1,11 @@
+public class dowhileDef{
+    public static void main(String[] args){
+        int i = 0;
+        do{
+            System.out.println(i);
+            i++;//The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true. Then it will repeat the loop as long as the condition is true
+        }
+        while(i < 5);//The semicolon ; after the while condition is required!
+    }
+
+}
