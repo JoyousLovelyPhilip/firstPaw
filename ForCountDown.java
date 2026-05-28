4ee7d45f01b75878b91e6e0a5e7d6863d0b34002
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-28 23:58:56 +0530

    Add files via upload

---
+++ ForCountDown.java
@@ -0,0 +1,7 @@
+public class loo{
+    public static void main(String[] args){
+        for (int i= 20;i> 0; i=i-2){
+            System.out.println(i);// prints all even numbers between 20 to 0 in reverse
+        }
+    }
+}
