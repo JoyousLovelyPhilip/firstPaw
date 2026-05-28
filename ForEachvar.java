e6068fe4de4256866e538ec80f609e37655dfb06
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-29 00:38:17 +0530

    Add files via upload

---
+++ ForEachvar.java
@@ -0,0 +1,9 @@
+public class loo{
+    public static void main(String[] args){
+        String[] car = {"Benz","volvo","tata","Mahindra","Suzuki"};
+        for(String cars: car){
+            System.out.println(cars);//prints all cars
+        }//There is also a "for-each" loop, which is used exclusively to loop through elements in an array (or other data structures):
+        //The for-each loop is simpler and more readable than a regular for loop, since you don't need a counter (like i < array.length).
+    }
+}
