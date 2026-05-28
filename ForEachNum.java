a5260898833c15e744f18597a9b6cdbbe0171dd7
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-29 00:44:14 +0530

    Add files via upload

---
+++ ForEachNum.java
@@ -0,0 +1,8 @@
+public class numbu{
+    public static void main(String[]  args){
+        int[] num ={1,3,4,5,6};
+        for(int nums : num){
+            System.out.println(nums);
+        }// prints numbers
+    }
+}
