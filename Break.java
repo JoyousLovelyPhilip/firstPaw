b0e899136f1ea555b6813888ba4d0053e61b9e9c
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-29 00:59:46 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,10 @@
+public class numbu{
+    public static void main(String[]  args){
+        for(int i = 1;i <= 10;i++){
+            if(i==5){
+                break;//The break statement can also be used to jump out of a loop.
+            }
+            System.out.println(i);//[breaks loop at 5 and prints 1 2 3 4 ]
+        }
+    }  
+}
