289d76628517a2c178483b84fb1a97f260d60a69
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-29 00:51:56 +0530

    Add files via upload

---
+++ ForFact.java
@@ -0,0 +1,10 @@
+public class numbu{
+    public static void main(String[]  args){
+        int t = 6;
+        int fact = 1;
+        for(int i = 1; i<= t; i++){
+            fact *= i;
+            System.out.println(fact);//720
+        }
+    }  
+}
