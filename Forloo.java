8f8c23964edc5d4f8eb41556133f0b92b7204ec8
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-28 23:55:41 +0530

    Add files via upload

---
+++ Forloo.java
@@ -0,0 +1,7 @@
+public class loo{
+    public static void main(String[] args){
+        for (int i= 0;i <= 20; i=i+2){
+            System.out.println(i);// prints all even numbers between 0 to 20
+        }
+    }
+}
