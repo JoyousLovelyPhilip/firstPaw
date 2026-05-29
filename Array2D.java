2f858e13c9d0c324b667b3614b3f39b04e248d25
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-30 00:55:17 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,8 @@
+public class array2d{
+    public static void main(String[] args){
+        int[][] nums ={{2,3,4},{7,8,9}};
+        System.out.println(nums [0][1]);
+        nums [0][2] = 56;
+        System.out.println(nums [0][2]);
+    }
+}
