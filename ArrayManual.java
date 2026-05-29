eb0b82f139349bbdac087769c7da8f0f9520ecab
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-30 00:05:30 +0530

    Add files via upload

---
+++ ArrayManual.java
@@ -0,0 +1,8 @@
+public class jo{
+    public static void main(String[]  args){
+        int[] num ={1,34,5,6,67,8,78};
+        for (int i : num){
+            System.out.println(i);// this prints the entire array ina loop way...since array cannot be printd directly
+        }
+    }
+}
