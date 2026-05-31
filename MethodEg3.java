f76ea72e712c34e93e37a7f07b7d31b1fda88ffd
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-01 00:17:38 +0530

    Add files via upload

---
+++ MethodEg3.java
@@ -0,0 +1,9 @@
+public class joo{
+    static void Chall(String name){
+        System.out.println("Welcome dear " +name);
+    }
+    public static void main(String[] args){
+        Chall("Joy");
+        Chall("Annet");
+    }
+}
