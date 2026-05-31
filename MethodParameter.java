14195002780a65a55b59d5457a995e328bf1ec63
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-31 23:40:26 +0530

    Add files via upload

---
+++ MethodParameter.java
@@ -0,0 +1,11 @@
+public  class Para{
+    static void Mypara(String git){
+        System.out.println(git+ " Pandian");
+    }
+    public static void main(String[] args){
+        Mypara("Annet");
+        Mypara("Lovely");
+        Mypara("Joyous");
+        Mypara("Philip V");//When a parameter is passed to the method, it is called an argument. So, from the example above: fname is a parameter, while Lovely,philip v, Annet and joyous are arguments.
+    }
+}
