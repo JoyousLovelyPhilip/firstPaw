45a950c88caddd4a68742ad862b7c6d1a55acda1
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-31 23:46:16 +0530

    Add files via upload

---
+++ MethMulti.java
@@ -0,0 +1,11 @@
+public class multi{
+    static void MultiPara(String name, int age){
+        System.out.println(name + " is "+ age+ " years old");
+    }
+    public static void main(String[] args){
+        MultiPara("Joy", 19);
+        MultiPara("Annet", 20);
+        MultiPara("Amma", 45);
+        MultiPara("Appa",46);
+    }
+}
