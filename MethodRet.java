17162380b0e27f622c2aad213c83a3cd023dc70e
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-01 00:01:01 +0530

    Add files via upload

---
+++ MethodRet.java
@@ -0,0 +1,11 @@
+public class ret{
+    static int Return(int x, int y){
+        return x* y ;
+    }
+    public static void main(String[] args){
+        System.out.println(Return(12,4));
+        System.out.println(Return(3,5));
+        System.out.println(Return(4,7));
+
+    }
+}
