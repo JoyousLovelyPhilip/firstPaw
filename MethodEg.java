cc8026d59285a0c61fdfb54e0e07db1d09b0fc5a
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-31 23:29:33 +0530

    Add files via upload

---
+++ MethodEg.java
@@ -0,0 +1,9 @@
+public class joy{
+    static void Welcomenote(){
+    System.out.println("Welcome to Java!");
+    }
+    public static void main(String[] args){
+        Welcomenote();
+        Welcomenote();
+    }
+}
