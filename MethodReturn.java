fc45c531d4e783b7f88ac4b62f1801caefcbbc6f
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-31 23:58:52 +0530

    Add files via upload

---
+++ MethodReturn.java
@@ -0,0 +1,11 @@
+public class ret{
+    static int Return(int x){
+        return 3*x ;
+    }
+    public static void main(String[] args){
+        System.out.println(Return(12));
+        System.out.println(Return(3));
+        System.out.println(Return(4));
+
+    }
+}
