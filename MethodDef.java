653639961521e8d36c9f1b68615f6e540cb2bb80
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-05-31 23:21:57 +0530

    Add files via upload

---
+++ MethodDef.java
@@ -0,0 +1,10 @@
+public class joy{
+    static void MyJoy()// is the name of the method .A  method must be declared within a class. It is defined with the name of the method, followed by parentheses ().
+    {
+        System.out.println("The demo");
+    }
+
+    public static void main(String[] args){
+        MyJoy();
+    }
+}
