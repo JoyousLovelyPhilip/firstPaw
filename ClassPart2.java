ebf94a9fbb07d9ca5a0b8eb7cd30535548422d30
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-02 16:45:28 +0530

    Add files via upload

---
+++ ClassPart2.java
@@ -0,0 +1,10 @@
+public class ClassPart2{
+    
+    String go = "Jois";
+
+public static void main(String[] args)
+{
+        ClassPart2 goe = new ClassPart2();
+        System.out.println("My Name seh: " + goe.go);
+    }
+}
