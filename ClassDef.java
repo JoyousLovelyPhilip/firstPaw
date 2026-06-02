51c797ac87c724b7bf4a6d39b55911adbce04c35
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-02 15:54:30 +0530

    Add files via upload

---
+++ ClassDef.java
@@ -0,0 +1,8 @@
+public class ClassDef{
+    int x = 23;
+    public static void main(String[] args)
+    {
+        ClassDef Obj = new ClassDef();
+        System.out.println(Obj.x);//op: (obj)=ClassDef@4e718207, (obj.x)= 23
+    }
+}
