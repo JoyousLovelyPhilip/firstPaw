a67ad053455876929b93b6686cf64a92f893e845
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-03 23:54:52 +0530

    Add files via upload

---
+++ AttributeEg1.java
@@ -0,0 +1,9 @@
+public class Modify{
+    int x = 45;
+
+    public static void main(String[] args){
+        Modify box = new Modify();
+        box.x = 34;//op:34
+        System.out.println(box.x);
+    }
+}
