4349be740ae5243c179faa85f94d8c3aed47c958
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-03 23:49:39 +0530

    Add files via upload

---
+++ ClassAttribute.java
@@ -0,0 +1,10 @@
+public class ClassPart2{
+    
+    String go = "Jois";//In Java, variables declared inside a class are called "attributes".
+//Another name for attributes is fields.
+public static void main(String[] args)
+{
+        ClassPart2 goe = new ClassPart2();
+        System.out.println("My Name seh: " + goe.go);
+    }
+}
