2f8a485c014bd04dbf9e7883fb1f5b9c547bf721
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-04 14:36:55 +0530

    Add files via upload
    i have added multiple class files from multiclass folder

---
+++ Class1.java
@@ -0,0 +1,9 @@
+public class Class1{
+     void name(String Myname)//When methods are static, you don't need to create an object You could call them directly:
+     {
+        System.out.println("name"+ Myname);
+    }
+    void prof(){
+        System.out.println("Im a Blooming kid by heart, navigating through young Adulthood");
+    }
+}
---
+++ Class2.java
@@ -0,0 +1,7 @@
+public class Class2{ 
+    public static void main(String [] args){
+    Class1 box = new Class1();
+    box.name(" JoyousLovelyPhilip");
+    box.prof();
+}//hurray! op:
+}
