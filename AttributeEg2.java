4a9cd5708e75ab30ac5fe1543ce0f8f672d26d33
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-04 13:44:15 +0530

    Add files via upload

---
+++ AttributeEg2.java
@@ -0,0 +1,12 @@
+public class Atti{
+    int age =34;
+    String nam = "JOeee";
+    String nam1= "Annee";
+
+    public static void main (String [] args){
+        Atti Dab = new Atti();
+        Dab.nam = "Joy";
+        System.out.println("My NAme is "+ Dab.nam + " and my Sister is "+ Dab.nam1);//My NAme is: Joy and my Sister is Annee
+        System.out.println("my Age is "+ Dab.age);//my Age is 34
+    }
+}
