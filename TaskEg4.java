9035972755c1404489f61c17be4052e9c09414dc
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-06 00:01:49 +0530

    Add files via upload

---
+++ TaskEg4.java
@@ -0,0 +1,10 @@
+public class SquareNum{
+
+    static int square(int num){
+        return num*num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(7));
+    }
+}
