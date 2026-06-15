20855b5727dda3557db9958e0559230eb2c40b96
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-06-15 23:59:40 +0530

    Add files via upload

---
+++ ThisEg1.java
@@ -0,0 +1,22 @@
+public class ThisEg1{
+    int year;
+    String name;
+
+    public ThisEg1(int year){
+        this(2032, name );
+    }
+    public ThisEg1(int year, String name){
+        this.year= year;
+        this.name = name;
+    }
+    public void ThisEg1as(){
+        System.out.println(year + " , "+ name );
+    }
+    public static void main(String[] args){
+        ThisEg1 car1 = new ThisEg1("volsvogen");
+        ThisEg1 car2 = new ThisEg1(2004, "Joyous");
+
+        car1.ThisEg1as();
+        car2.ThisEg1as();
+    }
+}
