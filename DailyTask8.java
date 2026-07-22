fdd61593c8b69fc9d6cfdaaddacdcf9c58cdf888
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-07-22 21:17:28 +0530

    Add files via upload

---
+++ DailyTask8.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class DailyTask8{
+    public static void main(String[] args){
+        ArrayList<Integer> joy = new ArrayList <Integer>();
+        joy.add(45);
+        joy.add(56);
+        joy.add(67);
+        joy.add(90);
+
+        for(Integer i : joy){
+            System.out.println(i);
+        }
+        
+    }
+}
