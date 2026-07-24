a89621a62dee332050518930a258f0ce3b69f240
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-07-24 22:12:55 +0530

    Update DSAlgorithmSearching.java

--- DSAlgorithmSearching.java
+++ DSAlgorithmSearching.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class DSAlgorithmSearching{
     public static void main(String[] args){
-        var happy =new ArrayList<String>();
+        var happy =new ArrayList<String>();//you dont have to import ArrayList separately when you use var
         happy.add("Annet");
-        happy.add("Pandian");
+        happy.add("Pandian V");
         happy.add("Lovely");
         happy.add("Joyous");
 
