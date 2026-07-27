dc7ab88c1c57dc801fc859b310b9bd46df5af839
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-07-27 23:25:21 +0530

    Update useofFor.java

--- useofFor.java
+++ useofFor.java
@@ -1,7 +1,7 @@
 public class useofFor {
     public static void main(String[] args)
     {
-        for (int i =0; i <= 10; i++){
+        for (int i =0; i <= 20; i++){
             if(i%2 ==0){
                 System.out.println("Number is even");
             }
