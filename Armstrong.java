50af69906db22e20ea86a1044a6e7b9a510436ff
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-08-09 20:09:08 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -5,7 +5,7 @@ public class Armstrong{
         System.out.println("Enter the Number( note:- The number has to be a three digit number) :");
         int num = sc.nextInt();
         
-        int original = num;
+        int original = num; 
         int sum =0;
         while(num!= 0){
             int digit = num % 10;
