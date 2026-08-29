6d62c07cff8ae4455fe45f728cef25bdbc16a7f1
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-08-29 23:40:14 +0530

    Rename class loo to Forloo
    Renamed class from 'loo' to 'Forloo' and updated formatting.

--- Forloo.java
+++ Forloo.java
@@ -1,4 +1,4 @@
-public class loo{
+public class Forloo{
     public static void main(String[] args){
         for (int i= 0;i <= 20; i=i+2){
             System.out.println(i);// prints all even numbers between 0 to 20
