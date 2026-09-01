186c12a3758cf790fc9ff9cc6d42cf9cf2efb4f5
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-09-01 21:23:55 +0530

    Update ClassPart2.java

--- ClassPart2.java
+++ ClassPart2.java
@@ -1,10 +1,9 @@
 public class ClassPart2{
     
     String go = "Jois";
-
 public static void main(String[] args)
 {
         ClassPart2 goe = new ClassPart2();
-        System.out.println("My Name seh: " + goe.go);
+        System.out.println("My Nam seh: " + goe.go);
     }
 }
