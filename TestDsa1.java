59c2bf32002c3c09eb70ba39f1236a4ec3a269f0
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-09-09 22:28:32 +0530

    Update TestDsa1.java

--- TestDsa1.java
+++ TestDsa1.java
@@ -5,6 +5,26 @@ public class TestDsa1{
         System.out.println("Enter the Set of Numbers: ");
         int n = sc.nextInt();
         int[] arr = new int(n);
+        System.out.println("Enter "+n +" elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int index = 0;
+        for (int i = 0; i < n; i++){
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+        System.out.println("Array after moving zeros:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
 
     }
 }
