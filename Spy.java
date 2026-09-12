63884bd064e3787153fbfbe73f6c93b5752891af
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-09-12 22:36:24 +0530

    Update Spy.java

--- Spy.java
+++ Spy.java
@@ -19,5 +19,10 @@ public class Spy{
             System.out.println("It is not a Spy Number");
         }
         sc.close();
+        //[public class HelloWorld {
+    //public static void main(String[] args) {
+        //System.out.println("Hello, World!");]
+    }
+}
     }
 }
