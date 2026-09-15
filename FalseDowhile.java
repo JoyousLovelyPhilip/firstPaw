ac41f5c30391ad0a471bf65e3375442f1489c2b4
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-09-15 21:09:03 +0530

    Add files via upload

---
+++ FalseDowhile.java
@@ -0,0 +1,9 @@
+public class FalseDoWhile{
+    public static void main(String[] args){
+        int joy = 10;
+        do{
+            System.out.println("Hi! My name is Joy");
+        }
+        while(joy < 7);// tho the condition is false the do 
+    }
+}
