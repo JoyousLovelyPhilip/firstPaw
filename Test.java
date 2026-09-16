94b0bdb1605dd0987fa99862673b565a9f86f6f5
JoyousLovelyPhilip <vpjoyousvpjoyous@gmail.com>
2026-09-16 22:47:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String[] args){
+        int i = 1;
+        while(i<=20){
+            System.out.println(i);
+            i++;
+            // i used "break" here the o/p was : 1...when i removed break it printed 1 to 20
+        }
+    }
+}
